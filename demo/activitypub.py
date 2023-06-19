51b61d2db7c5be04167b7c2b0b93130a44ef65ad
SouthFox <master@southfox.me>
2023-06-19 10:03:31 +0800

    [feat] add fetch function

---
+++ demo/activitypub.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""ActivityPub settings, fetch & post"""
+import httpx
+import config
+
+
+def fetch(
+        url: str,
+) -> dict:
+    """Fetch url and return json"""
+    print(f"fetch {url}")
+
+    with httpx.Client() as client:
+        resp = client.get(
+            url,
+            headers={
+                "User-Agent": config.USER_AGENT,
+                "Accept": config.AP_CONTENT_TYPE,
+            }
+        )
+
+    return resp.json()

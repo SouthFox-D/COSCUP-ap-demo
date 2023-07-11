98f5e47dd852d8038364404c7ee5f183818584c8
SouthFox <master@southfox.me>
2023-07-12 00:12:00 +0800

    [feat] gen key script

---
+++ misc.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Something misc."""
+import click
+import sys
+
+from Crypto.PublicKey import RSA
+from demo import config
+
+
+@click.command()
+def gen_key():
+    """Generate key."""
+    if config.KEY_PATH.exists():
+        print("Key is existing!")
+        sys.exit(2)
+    else:
+        k = RSA.generate(2048)
+        privkey_pem = k.exportKey("PEM").decode("utf-8")
+        config.KEY_PATH.write_text(privkey_pem)
+        print("Done!")
+
+
+if __name__ == "__main__":
+    gen_key()
